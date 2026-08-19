89d981b2bbb5ac9d9ec27a2ecdd43f7ad669e225
JonathanAlessandro <jonathan.alessandro212@gmail.com>
2026-08-19 14:08:29 -0300

    fix: remove instrucao duplicada da migracao

--- drizzle/0002_flat_wrecker.sql
+++ drizzle/0002_flat_wrecker.sql
@@ -1,4 +1,3 @@
-ALTER TABLE `documents` DROP FOREIGN KEY `documents_createdByUserId_users_id_fk`;
 --> statement-breakpoint
 ALTER TABLE `documents` DROP FOREIGN KEY IF EXISTS `documents_createdByUserId_users_id_fk`;--> statement-breakpoint
 ALTER TABLE `documents` MODIFY COLUMN `createdByUserId` int;--> statement-breakpoint
